c55842065418b23dc9c98af0e9b0746206d2d553
Kristil Erla Håland <78584348+kristilerla@users.noreply.github.com>
2024-04-21 20:54:01 +0200

    added api data for weather

--- src/app.js
+++ src/app.js
@@ -3,26 +3,31 @@ function refreshWeather (response){
     let temperatureElement = document.querySelector("#temperature");
    let temperature = response.data.temperature.current;
    let cityElement = document.querySelector("#city");
-   cityElement.innerHTML = response.data.city;
+   let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+ console.log(response.data.temperature.humidity);
 
+
+    cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     temperatureElement.innerHTML = Math.round(temperature);
-
-
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 }
 
 function searchCity(city) {
 let apiKey="4ff060ct0o35c409303b648f04f0ab31";
 let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 axios.get(apiUrl).then(refreshWeather);
-
 }
 
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  
+
   searchCity (searchInput.value);
 }
 
