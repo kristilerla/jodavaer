65ec126ae7cd9edfba49eafdf724fb1944a1e0c0
Kristil Erla Håland <78584348+kristilerla@users.noreply.github.com>
2024-04-22 17:31:52 +0200

    trying to fix lost api key by fixing city element

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@
 function refreshWeather (response){
     let temperatureElement = document.querySelector("#temperature");
    let temperature = response.data.temperature.current;
-   let cityElement = document.querySelector("#city");
+  
    let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windSpeedElement = document.querySelector("#wind-speed");
- console.log(response.data.temperature.humidity);
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+
 
 
     cityElement.innerHTML = response.data.city;
@@ -14,11 +16,14 @@ function refreshWeather (response){
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     temperatureElement.innerHTML = Math.round(temperature);
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = `${date.getHours()}:${date.getMinutes()};
+
 }
 
 function searchCity(city) {
 let apiKey="4ff060ct0o35c409303b648f04f0ab31";
 let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
 axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -26,8 +31,8 @@ axios.get(apiUrl).then(refreshWeather);
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  
-
+   let cityElement = document.querySelector("#city");
+ cityElement.innerHTML = response.data.city;
   searchCity (searchInput.value);
 }
 
