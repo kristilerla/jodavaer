cb62623ed4533b7a7327de815d246b144097cf99
Kristil Erla Håland <78584348+kristilerla@users.noreply.github.com>
2024-04-24 21:59:03 +0200

    changed the css and the forecaste content

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   timeElement.innerHTML = formatDate(date);
 
+getForecast(response.data.city);
 
 }
 
@@ -33,10 +34,7 @@ if (minutes < 10) {
 }
 
 return `${day}, ${hours}:${minutes}`;
-
-
 }
-
 function searchCity(city) {
   let apiKey = "4ff060ct0o35c409303b648f04f0ab31";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
@@ -50,36 +48,56 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+let date = new Date (timestamp * 1000)
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-function displayForecast() {
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+return days[date.getDay()];
+
+
+}
+
+function getForecast(city) {
+let apiKey = "4ff060ct0o35c409303b648f04f0ab31";
+let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
   let forecastHtml = "";
 
-  days.forEach(function (day) {
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5){
+
     forecastHtml =
       forecastHtml +
-      ` <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">🌤️</div>
+      `
+       <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <div class="weather-forecast-icon">
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        </div>
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
-            <strong>15º </strong>
+            <strong>${Math.round(day.temperature.maximum)}º </strong>
           </div>
-          <div class="weather-forecast-temperature"> 9º</div>
+          <div class="weather-forecast-temperature"> ${Math.round(day.temperature.minimum)}º</div>
         </div>
       </div>
     `;
+    }
   });
 
-  let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = forecastHtml;
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
 }
+
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Oslo");
-displayForecast();
+
 
 
 
